c50ed5723db49b40c74bb5f1ae231a694c0cc08e
엄태건 <umtimos@naver.com>
2023-03-17 00:46:52 +0900

    엄태건 : 포켓몬데이터베이스 함수 생성

--- APIApp/GameEngineContents/PokeDataBase.cpp
+++ APIApp/GameEngineContents/PokeDataBase.cpp
@@ -1,5 +1,7 @@
 #include "PokeDataBase.h"
 
+#include <GameEngineBase/GameEngineRandom.h>
+
 PokeDataBase::PokeDataBase() 
 {
 }
@@ -8,9 +10,37 @@ PokeDataBase::~PokeDataBase()
 {
 }
 
-void PokeDataBase::PokeCreate()
+void PokeDataBase::PokeCreate(PokeNumber _Number, int _Level)
 {
-	
+	PokeDexNumber = _Number;
+	switch (PokeDexNumber)
+	{
+	case PokeNumber::Bulbasaur:
+		BulbasaurData(_Level);
+		break;
+	case PokeNumber::Ivysaur:
+		break;
+	case PokeNumber::Venusaur:
+		break;
+	case PokeNumber::Charmander:
+		CharmanderData(_Level);
+		break;
+	case PokeNumber::Charmeleon:
+		break;
+	case PokeNumber::Charizard:
+		break;
+	case PokeNumber::Squirtle:
+		SquirtleData(_Level);
+		break;
+	case PokeNumber::Wartortle:
+		break;
+	case PokeNumber::Blastoise:
+		break;
+	case PokeNumber::Max:
+		break;
+	default:
+		break;
+	}
 }
 
 // 몬스터를 처치하면 경험치 획득
@@ -40,14 +70,14 @@ void PokeDataBase::PokeLevelUp(int _EXP)
 }
 
 // 레벨업을 하면 체력, 공격력, 방어력, 특수공격력, 특수방어력, 민첩성이 증가
-void PokeDataBase::PokeStatusUp()
+void PokeDataBase::PokeStatusUp(int _Status)
 {
-	HealthPoint += 1;                       
-	AttackPower += 1;                       
-	Defense += 1;                           
-	SpecialAttackPower += 1;                
-	SpecialDefense += 1;                    
-	Agility += 1;                           
+	HealthPoint += _Status;
+	AttackPower += _Status;
+	Defense += _Status;
+	SpecialAttackPower += _Status;
+	SpecialDefense += _Status;
+	Agility += _Status;
 }
 
 // 진화 조건을 충족 시 몬스터 진화
@@ -60,4 +90,108 @@ void PokeDataBase::PokeEvolution()
 void PokeDataBase::PokeSkillAcquisition()
 {
 	// 파이리의 레벨이 몇 일때 무슨 스킬을 획득 가능
+}
+
+// 성별 결정
+void PokeDataBase::GenderDecision()
+{
+	int RandValue = GameEngineRandom::MainRandom.RandomInt(1, 2);
+
+	if (1 == RandValue)
+	{
+		IsMan = true;  // 수컷
+	}
+	else
+	{
+		IsMan = false; // 암컷
+	}
+}
+
+// 성격 결정
+PokePersonality PokeDataBase::PersonalityDecision()
+{
+	int RandValue = GameEngineRandom::MainRandom.RandomInt(0, 24);
+
+	Personality = static_cast<PokePersonality>(RandValue);
+}
+
+void PokeDataBase::BulbasaurData(int _Level)
+{
+	GenderDecision();
+	PersonalityDecision();
+	Type = PokeType::Grass;
+	Characteristic = PokeCharacteristic::심록;
+	
+	HealthPoint = 20;
+	AttackPower = 15;
+	Defense = 15;
+	SpecialAttackPower = 15;
+	SpecialDefense = 15;
+	Agility = 15;
+
+	int PlusLevel = MonsterLevel + _Level;
+
+	for (int i = 0; i < PlusLevel; i++)
+	{
+		PokeStatusUp(1);
+	}
+
+	// 스킬 부여
+	// SkillList[0].~ = "몸통박치기";
+	// SkillList[1].~ = "울음소리";
+	// SkillList[2].~ = "잎날리기";
+}
+
+void PokeDataBase::CharmanderData(int _Level)
+{
+	GenderDecision();
+	PersonalityDecision();
+	Type = PokeType::Fire;
+	Characteristic = PokeCharacteristic::맹화;
+
+	HealthPoint = 25;
+	AttackPower = 12;
+	Defense = 12;
+	SpecialAttackPower = 12;
+	SpecialDefense = 12;
+	Agility = 12;
+
+	int PlusLevel = MonsterLevel + _Level;
+
+	for (int i = 0; i < PlusLevel; i++)
+	{
+		PokeStatusUp(2);
+	}
+
+	// 스킬부여
+	// SkillList[0].~ = "몸통박치기";
+	// SkillList[1].~ = "울음소리";
+	// SkillList[2].~ = "불꽃세례";
+}
+
+void PokeDataBase::SquirtleData(int _Level)
+{
+	GenderDecision();
+	PersonalityDecision();
+	Type = PokeType::Water;
+	Characteristic = PokeCharacteristic::급류;
+
+	HealthPoint = 30;
+	AttackPower = 10;
+	Defense = 10;
+	SpecialAttackPower = 10;
+	SpecialDefense = 10;
+	Agility = 10;
+
+	int PlusLevel = MonsterLevel + _Level;
+
+	for (int i = 0; i < PlusLevel; i++)
+	{
+		PokeStatusUp(3);
+	}
+
+	// 스킬부여
+	// SkillList[0].~ = "몸통박치기";
+	// SkillList[1].~ = "울음소리";
+	// SkillList[2].~ = "물대포";
 }
--- APIApp/GameEngineContents/PokeDataBase.h
+++ APIApp/GameEngineContents/PokeDataBase.h
@@ -3,6 +3,81 @@
 #include <string>
 #include <vector>
 
+// 도감번호는 +1로 표시
+enum class PokeNumber
+{
+	Bulbasaur,  // 0 이상해씨
+	Ivysaur,    // 1 이상해풀
+	Venusaur,   // 2 이상해꽃
+	Charmander, // 3 파이리
+	Charmeleon, // 4 리자드
+	Charizard,  // 5 리자몽
+	Squirtle,   // 6 꼬부기
+	Wartortle,  // 7 어니부기
+	Blastoise,  // 8 거북왕
+	Max,        // max
+};
+
+// 포켓몬 타입
+enum class PokeType
+{
+	Normal,
+	Fight,
+	Flying,
+	Poision,
+	Bug,
+	Ground,
+	Rock,
+	Ghost,
+	Steel,
+	Fire,
+	Water,
+	Grass,
+	Electro,
+	Ice,
+	Psvchc,
+	Dragon,
+	Drak,
+};
+
+// 포켓몬 성격
+enum class PokePersonality
+{
+	Lonely,
+	Adamant,
+	Naughty,
+	Brave,
+	Bold,
+	Impish,
+	Lax,
+	Relaxed,
+	Modest,
+	Mild,
+	Rash,
+	Quiet,
+	Calm,
+	Gentle,
+	Careful,
+	Sassy,
+	Timid,
+	Hasty,
+	Jolly,
+	Naive,
+	Bashful,
+	Hardy,
+	Docile,
+	Quirky,
+	Serious,
+};
+
+// 포켓몬 특성
+enum class PokeCharacteristic
+{
+	심록,
+	맹화,
+	급류,
+};
+
 class GameEngineRender;
 class PokeSkillBase;
 
@@ -20,10 +95,10 @@ public:
 	PokeDataBase& operator=(const PokeDataBase& _Other) = delete;
 	PokeDataBase& operator=(PokeDataBase&& _Other) noexcept = delete;
 
-	void PokeCreate();
+	void PokeCreate(PokeNumber _Number, int _Level);
 	void PokeExperienceAcquisition(int _EXP);
 	void PokeLevelUp(int _EXP);
-	void PokeStatusUp();
+	void PokeStatusUp(int _Status);
 	void PokeEvolution();
 	void PokeSkillAcquisition();
 
@@ -31,31 +106,41 @@ protected:
 
 private:
 	// 포켓몬 데이터
-
-	GameEngineRender* MonsterImage = nullptr;  // 포켓몬 이미지
+	GameEngineRender* MonsterImage = nullptr;						 // 포켓몬 이미지
 	// 기술 4개 벡터
 
+	std::string Name = "안농";									     // 포켓몬 이름
+											    
+	bool IsMan = true;											     // 포켓몬 성별
+	bool IsbeCaught = false;									     // 야생포켓몬인지 잡힌 포켓몬인지
+
+	PokeNumber PokeDexNumber = PokeNumber::Max;					     // 포켓몬 도감 번호
+	int HealthPoint = 0;										     // 몬스터 체력
+	int AttackPower = 0;											 // 몬스터 공격력
+	int Defense = 0;												 // 몬스터 방어력
+	int SpecialAttackPower = 0;										 // 몬스터 특수공격력
+	int SpecialDefense = 0;											 // 몬스터 특수방어력
+	int Agility = 0;											     // 몬스터 민첩성
+	int MonsterLevel = 0;										     // 몬스터 레벨
+	int Experience = 0;												 // 몬스터 경험치
+	int RemainingExperience = 0;								     // 몬스터 다음 레벨까지 남은 경험치
+											    
+	int PokeBall = 0;												 // 포켓몬이 담겨있는 포켓볼 종류
+	PokeType Type = PokeType::Normal;								 // 포켓몬 속성 타입
+	PokePersonality Personality = PokePersonality::Serious;			 // 포켓몬 성격
+	PokeCharacteristic Characteristic = PokeCharacteristic::심록;	 // 포켓몬 특성
+
+	// 성격, 성별 난수
+	PokePersonality PersonalityDecision();
+	void GenderDecision();
+
+
+	// 포켓몬 베이스 데이터
+	void BulbasaurData(int _Level);
+	void CharmanderData(int _Level);
+	void SquirtleData(int _Level);
+
+	// 스킬
 	std::vector<PokeSkillBase*> SkillList = std::vector<PokeSkillBase*>(4);
-	std::string Name = "안농";                  // 포켓몬 이름
-
-	bool IsMan = true;                         // 포켓몬 성별
-	bool IsbeCaught = false;                   // 야생포켓몬인지 잡힌 포켓몬인지
-
-	int PokeDexNumber = 0;                     // 포켓몬 도감 번호
-	int HealthPoint = 0;                       // 몬스터 체력
-	int AttackPower = 0;                       // 몬스터 공격력
-	int Defense = 0;                           // 몬스터 방어력
-	int SpecialAttackPower = 0;                // 몬스터 특수공격력
-	int SpecialDefense = 0;                    // 몬스터 특수방어력
-	int Agility = 0;                           // 몬스터 민첩성
-	int MonsterLevel = 0;                      // 몬스터 레벨
-	int Experience = 0;						   // 몬스터 경험치
-	int RemainingExperience = 0;               // 몬스터 다음 레벨까지 남은 경험치
-
-	int PokeBall = 0;                          // 포켓몬이 담겨있는 포켓볼 종류
-	int Type = 0;							   // 포켓몬 속성 타입
-	int Personality = 0;                       // 포켓몬 성격
-	int Characteristic = 0;					   // 포켓몬 특성
-
 };
 
