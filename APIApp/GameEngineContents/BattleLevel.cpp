1a686dac0f344b0342e4aed9e8696aeba56a24e7
KyungUnMin <insetour@naver.com>
2023-03-16 13:47:40 +0900

    민경운 : Battle레벨 준비

---
+++ APIApp/GameEngineContents/BattleLevel.cpp
@@ -0,0 +1,11 @@
+#include "BattleLevel.h"
+
+BattleLevel::BattleLevel()
+{
+
+}
+
+BattleLevel::~BattleLevel()
+{
+
+}
---
+++ APIApp/GameEngineContents/BattleLevel.h
@@ -0,0 +1,20 @@
+#pragma once
+
+
+class BattleLevel
+{
+public:
+	BattleLevel();
+	~BattleLevel();
+
+	BattleLevel(const BattleLevel& _Other) = delete;
+	BattleLevel(BattleLevel&& _Other) noexcept = delete;
+	BattleLevel& operator=(const BattleLevel& _Other) = delete;
+	BattleLevel& operator=(const BattleLevel&& _Other) noexcept = delete;
+
+protected:
+
+private:
+
+};
+
