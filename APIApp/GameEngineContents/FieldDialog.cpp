160420b23f8178042a518b6520955f45a5c00f92
승민 황 <iqmin11@gmail.com>
2023-03-17 13:16:15 +0900

    황승민 : FieldDialog 업데이트

--- APIApp/GameEngineContents/FieldDialog.cpp
+++ APIApp/GameEngineContents/FieldDialog.cpp
@@ -1,9 +1,11 @@
 #include "FieldDialog.h"
 #include <GameEngineCore/GameEngineRender.h>
 
+FieldDialog* FieldDialog::AcFeidDialog = nullptr;
+
 FieldDialog::FieldDialog()
 {
-
+	AcFeidDialog = this;
 }
 
 FieldDialog::~FieldDialog()
@@ -16,4 +18,55 @@ void FieldDialog::Start()
 	FieldDialogFrame = CreateRender("Npc_TextFrame.bmp", 0);
 	FieldDialogFrame->SetScaleToImage();
 	SetPos(ActorPos);
+
+	FieldDialogTextRender.resize(2);
+	FieldDialogTextRender[0].resize(30);
+	FieldDialogTextRender[1].resize(30);
+
+	for (size_t y = 0; y < FieldDialogTextRender.size(); y++)
+	{
+		for (size_t x = 0; x < FieldDialogTextRender[y].size(); x++)
+		{
+			FieldDialogTextRender[y][x] = CreateRender("Font_eng.bmp",1);
+			FieldDialogTextRender[y][x]->SetPosition(FirstTextRenderPos + float4{ static_cast<float>(x),static_cast<float>(y) } * (TextRenderInterval + TextRenderImageScale));
+			FieldDialogTextRender[y][x]->SetScale(TextRenderImageScale);
+			FieldDialogTextRender[y][x]->SetFrame(SpaceFrameNum);
+		}
+	}
+}
+
+void FieldDialog::Update(float _DeltaTime)
+{
+	StringToRender();
+}
+
+void FieldDialog::StringToRender()
+{
+	size_t StrEndIndex = Str.size()-1;
+	for (size_t y = 0; y < FieldDialogTextRender.size(); y++)
+	{
+		for (size_t x = 0; x < FieldDialogTextRender[y].size(); x++)
+		{
+			if (StrEndIndex < x + y*30 || ' ' == Str[x + y])
+			{
+				FieldDialogTextRender[y][x]->SetFrame(SpaceFrameNum);
+			}
+			else
+			{
+				if (Str[x + y * 30] >= 'A' && Str[x + y * 30] <= 'Z')
+				{
+					FieldDialogTextRender[y][x]->SetFrame(Str[x + y * 30] - 'A');
+				}
+				else if (Str[x + y * 30] >= 'a' && Str[x + y * 30] <= 'z')
+				{
+					FieldDialogTextRender[y][x]->SetFrame(Str[x + y * 30] - 'a' + 27);
+				}
+			}
+
+			//if (' ' == Str[x + y])
+			//{
+			//	FieldDialogTextRender[y][x]->SetFrame(SpaceFrameNum);
+			//}
+		}
+	}
 }
--- APIApp/GameEngineContents/FieldDialog.h
+++ APIApp/GameEngineContents/FieldDialog.h
@@ -17,11 +17,23 @@ public:
 
 protected:
 	void Start() override;
+	void Update(float _DeltaTime) override;
 
 private:
 	
+	static FieldDialog* AcFeidDialog;
+
 	float4 ActorPos = {480,544};
 	GameEngineRender* FieldDialogFrame = nullptr;
 
+	std::vector<std::vector<GameEngineRender*>> FieldDialogTextRender = std::vector<std::vector<GameEngineRender*>>();
+	float4 TextRenderImageScale = { 24, 48 };
+	float4 FirstTextRenderPos = { -388,-28 }; // 오른쪽으로 28 아래로 24
+	float4 TextRenderInterval = { 0,12 };
+	int SpaceFrameNum = 26;
+
+	std::string Str = "Hi My Name is HwangSeungMin";
+	
+	void StringToRender();
 };
 
