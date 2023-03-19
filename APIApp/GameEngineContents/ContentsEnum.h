4518d94d2b825cf8d38cb4d3755f7c4ac6611386
DESKTOP-QCG6R4C\rudtl <rudtlrdl96@gmail.com>
2023-03-19 10:06:42 +0900

    김경식 : 필드맵 타입 이미지 업로드

--- APIApp/GameEngineContents/ContentsEnum.h
+++ APIApp/GameEngineContents/ContentsEnum.h
@@ -22,7 +22,13 @@ enum class CollisionOrder
 
 enum class GroundType
 {
-	Grass,
-	Rock,
-	Water
+	Grass,  // c0f890 [r] 192 [g] 248 [b] 144
+	Rock,   // c8c058 [r] 200 [g] 192 [b] 88
+	Water,  // 70b8f0 [r] 112 [g] 184 [b] 240
+	Blue,   // c8e0e8 [r] 200 [g] 224 [b] 232
+	Grey,   // e8e8f0 [r] 232 [g] 232 [b] 240
+	Gold,   // e8e0c0 [r] 232 [g] 224 [b] 192
+	Purple, // d8b8e8 [r] 216 [g] 184 [b] 232
+	Cyan,   // b8e0d0 [r] 184 [g] 224 [b] 208
+	Beige   // e8e8e8 [r] 232 [g] 232 [b] 232
 };
