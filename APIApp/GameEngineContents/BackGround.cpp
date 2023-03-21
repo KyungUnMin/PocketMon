09c4961c67519458bb48ce9afb1c0c072a35839a
엄태건 <umtimos@naver.com>
2023-03-21 15:57:34 +0900

    엄태건 : 디버그 오류 수정 (BackGround.cpp_

--- APIApp/GameEngineContents/BackGround.cpp
+++ APIApp/GameEngineContents/BackGround.cpp
@@ -1,7 +1,7 @@
 #include "BackGround.h"
 #include <GameEnginePlatform/GameEngineWindow.h>
 #include <GameEngineCore/GameEngineRender.h>
-#include "ContentsEnums.h"
+#include "ContentsEnum.h"
 
 BackGround::BackGround() 
 {
@@ -14,7 +14,7 @@ BackGround::~BackGround()
 void BackGround::Start() 
 {
 	{
-		GameEngineRender* AnimationRender = CreateRender(RenderOrder::Background);
+		GameEngineRender* AnimationRender = CreateRender(RenderOrder::BackGround);
 		AnimationRender->SetPosition(GameEngineWindow::GetScreenSize().half());
 		AnimationRender->SetScale(GameEngineWindow::GetScreenSize());
 		AnimationRender->CreateAnimation({ .AnimationName = "Loop",  .ImageName = "BackGround.bmp", .Start = 0, .End = 2, .InterTime = 0.5f });
