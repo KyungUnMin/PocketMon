30de11abc118e8a23e6ca05781e155a0a3aec01d
Minseok Kim <mskim6800@naver.com>
2023-03-22 14:30:08 +0900

    김민석 : 전투UI 업데이트

--- APIApp/GameEngineContents/BackTextActor.cpp
+++ APIApp/GameEngineContents/BackTextActor.cpp
@@ -3,6 +3,7 @@
 #include <GameEnginePlatform/GameEngineWindow.h>
 #include <GameEngineCore/GameEngineRender.h>
 #include <GameEngineCore/GameEngineLevel.h>
+#include <GameEnginePlatform/GameEngineInput.h>
 #include "ContentsEnum.h"
 #include "TestScript.h"
 
@@ -26,7 +27,200 @@ void BackTextActor::Start()
 	RenderPtr->SetScale((RenderPtr->GetImage()->GetImageScale()));
 
 	ScriptPtr = GetLevel()->CreateActor<TestScript>();
+
+	BattleTextRender.resize(LineCount);
+	BattleTextRender[0].resize(OneLineSize);
+	BattleTextRender[1].resize(OneLineSize);
+
+	for (size_t y = 0; y < BattleTextRender.size(); y++)
+	{
+		for (size_t x = 0; x < BattleTextRender[y].size(); x++)
+		{
+			BattleTextRender[y][x] = CreateRender("Font_Dialog.bmp", RenderOrder::Battle_Text);
+			BattleTextRender[y][x]->SetPosition(FirstTextRenderPos + float4{ static_cast<float>(x),static_cast<float>(y) } *(TextRenderInterval + TextRenderImageScale));
+			BattleTextRender[y][x]->SetScale(TextRenderImageScale);
+			BattleTextRender[y][x]->SetFrame(SpaceFrameNum);
+			//BattleTextRender[y][x]->Off();
+			BattleTextRender[y][x]->EffectCameraOff();
+		}
+	}
+
+	TestText.push_back("it is test");
+	TestText.push_back("it is test2");
+
 }
+
+void BackTextActor::PushScriptBegin(std::list<std::string>::iterator _Begin)
+{
+	TestTextIter = _Begin;
+}
+
+void BackTextActor::PushScriptEnd(std::list<std::string>::iterator _End)
+{
+	TestTextEndIter = _End;
+}
+
+
+
+void BackTextActor::StringToRender()
+{
+	const std::string_view& Str = TestTextIter->data();
+
+	size_t StrEndIndex = Str.size() - 1;
+
+	int StrIndex = 0;
+
+	for (size_t y = 0; y < BattleTextRender.size(); y++) {
+		for (size_t x = 0; x < BattleTextRender[y].size(); x++) {
+
+			if (StrEndIndex < StrIndex || ' ' == Str[StrIndex])
+			{
+				BattleTextRender[y][x]->SetFrame(SpaceFrameNum);
+			}
+			else
+			{
+				if (Str[StrIndex] >= 'A' && Str[StrIndex] <= 'Z')
+				{
+					BattleTextRender[y][x]->SetFrame(Str[StrIndex] - 'A');
+				}
+				else if (Str[StrIndex] >= 'a' && Str[StrIndex] <= 'z')
+				{
+					BattleTextRender[y][x]->SetFrame(Str[StrIndex] - 'a' + 27);
+				}
+				else if (Str[StrIndex] >= '0' && Str[StrIndex] <= '9')
+				{
+					BattleTextRender[y][x]->SetFrame(Str[StrIndex] - '0' + 54);
+				}
+				else if (Str[StrIndex] == '\n')
+				{
+					while (x < BattleTextRender[y].size())
+					{
+						BattleTextRender[y][x]->SetFrame(SpaceFrameNum);
+						x++;
+					}
+				}
+				else
+				{
+					switch (Str[StrIndex])
+					{
+					case '!':
+						BattleTextRender[y][x]->SetFrame(81);
+						break;
+					case '?':
+						BattleTextRender[y][x]->SetFrame(82);
+						break;
+					case '/':
+						BattleTextRender[y][x]->SetFrame(85);
+						break;
+					case '-':
+						BattleTextRender[y][x]->SetFrame(86);
+						break;
+						//case '…':
+						//	FieldDialogTextRender[y][x]->SetFrame(91);
+						//	break;
+					case '.':
+						BattleTextRender[y][x]->SetFrame(92);
+						break;
+					case ',':
+						BattleTextRender[y][x]->SetFrame(93);
+						break;
+					case '@':
+						BattleTextRender[y][x]->SetFrame(94);
+						break;
+					default:
+						MsgAssert("아직 생각해보지 않은 글자입니다.");
+						break;
+					}
+				}
+			}
+			StrIndex++;
+		}
+	}
+	LastTextRenderIndex = FindLastTextRenderIndex();
+
+}
+
+void BackTextActor::ClearDialog()
+{
+	for (size_t y = 0; y < BattleTextRender.size(); y++)
+	{
+		for (size_t x = 0; x < BattleTextRender[y].size(); x++)
+		{
+			BattleTextRender[y][x]->SetFrame(SpaceFrameNum);
+			BattleTextRender[y][x]->Off();
+		}
+	}
+}
+
+
+int2 BackTextActor::FindLastTextRenderIndex()
+{
+	int2 SaveLastRenderIndex = { 0,0 };
+	for (int y = 0; y < BattleTextRender.size(); y++)
+	{
+		for (int x = 0; x < BattleTextRender[y].size(); x++)
+		{
+			if (SpaceFrameNum != BattleTextRender[y][x]->GetFrame())
+			{
+				SaveLastRenderIndex = { x,y };
+			}
+		}
+	}
+	return SaveLastRenderIndex;
+}
+void BackTextActor::Update(float _DeltaTime)
+{
+	PushStart(&TestText);
+	StringToRender();
+	for (size_t i = 0; i < FirstLineRenderLen; i++)
+	{
+		BattleTextRender[0][i]->On();
+	}
+
+	for (size_t i = 0; i < SecondLineRenderLen; i++)
+	{
+		BattleTextRender[1][i]->On();
+	}
+
+	if (BattleTextRender[LastTextRenderIndex.y][LastTextRenderIndex.x]->IsUpdate() && GameEngineInput::IsDown("A")) // 말걸었을때 바로 넘어가지 않게
+	{
+		TestTextIter++;
+		if (TestTextIter != TestTextEndIter)
+		{
+			ClearDialog();
+			FirstLineRenderLen = 0;
+			SecondLineRenderLen = 0;
+			StringToRender();
+		}
+	
+	}
+
+}
+
+void BackTextActor::PushStart(std::list<std::string>* _Text)
+{
+	PushScriptBegin(_Text->begin());
+	PushScriptEnd(_Text->end());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void BackTextActor::PushTexts(const std::vector<std::string_view>& _Texts)
 {
--- APIApp/GameEngineContents/BackTextActor.h
+++ APIApp/GameEngineContents/BackTextActor.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <GameEngineCore/GameEngineActor.h>
-
+#include "int2.h"
 class TestScript;
 
 class BackTextActor : public GameEngineActor
@@ -19,8 +19,41 @@ public:
 
 protected:
 	void Start() override;
+	void Update(float _DeltaTime) override;
+	void PushStart(std::list<std::string>* _Text);
+	void PushScriptBegin(std::list<std::string>::iterator _Begin);
+	void PushScriptEnd(std::list<std::string>::iterator _End);
+
+	void ClearDialog();
+
+	void StringToRender();
+
 
 private:
+
+
+	std::vector<std::vector<GameEngineRender*>> BattleTextRender = std::vector<std::vector<GameEngineRender*>>();
+	int OneLineSize = 30;
+	int LineCount = 2;
+	int SpaceFrameNum = 26;
+
+	int FirstLineRenderLen = 0;
+	int SecondLineRenderLen = 0;
+
+	std::list<std::string> TestText = std::list<std::string>();
+	std::list<std::string>::iterator TestTextIter = std::list<std::string>::iterator();
+	std::list<std::string>::iterator TestTextEndIter = std::list<std::string>::iterator();
+
+
+	float4 TextRenderImageScale = { 24, 48 };
+	float4 FirstTextRenderPos = { -420,-28 };
+	float4 TextRenderInterval = { 0,12 };
+
+	int2 LastTextRenderIndex = { -1,-1 };
+	int2 FindLastTextRenderIndex();
+
+
+
 	int CurIndex = 0;
 	std::vector<std::string> AllTexts;
 	TestScript* ScriptPtr = nullptr;
