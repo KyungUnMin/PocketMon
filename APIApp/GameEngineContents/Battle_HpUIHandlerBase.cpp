121e8930a2b0abdb1341edd34596ee35522b12a6
KyungUnMin <insetour@naver.com>
2023-03-30 17:04:22 +0900

    민경운 : 배틀 UI 경험치 적용

--- APIApp/GameEngineContents/Battle_HpUIHandlerBase.cpp
+++ APIApp/GameEngineContents/Battle_HpUIHandlerBase.cpp
@@ -1,5 +1,6 @@
 #include "Battle_HpUIHandlerBase.h"
 #include "BattleMonsterBase.h"
+#include "FriendlyHPBackground.h"
 
 Battle_HpUIHandlerBase::Battle_HpUIHandlerBase()
 {
@@ -9,6 +10,18 @@ Battle_HpUIHandlerBase::Battle_HpUIHandlerBase()
 Battle_HpUIHandlerBase::~Battle_HpUIHandlerBase()
 {
 
+}
+
+void Battle_HpUIHandlerBase::ExpCheck()
+{
+	FriendlyHPBackground* FriendHpUI = dynamic_cast<FriendlyHPBackground*>(UI);
+	if (nullptr == FriendHpUI)
+	{
+		MsgAssert("배틀 에러");
+		return;
+	}
+
+	FriendHpUI->IsExpUpCheck(true);
 }
 
 void Battle_HpUIHandlerBase::Update(float _DeltaTime)
