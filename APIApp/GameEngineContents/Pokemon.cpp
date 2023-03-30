997445227500f809b7cc1af8b20eb86fdcc7dce0
김경학 <51849541+onegoon123@users.noreply.github.com>
2023-03-30 12:43:51 +0900

    김경학 : Pokemon 클래스 생성, 필터 수정

---
+++ APIApp/GameEngineContents/Pokemon.cpp
@@ -0,0 +1,18 @@
+#include "Pokemon.h"
+
+Pokemon::Pokemon()
+{
+}
+
+Pokemon::~Pokemon() 
+{
+}
+
+void Pokemon::AddPokemon(PokeDataBase _PokeData)
+{
+}
+
+void Pokemon::AddPokemon(PokeNumber _Number, int _Level)
+{
+}
+
