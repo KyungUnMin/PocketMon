86b2ca3e576e4f019d786c1cac2b649e5156d8b6
KyungUnMin <insetour@naver.com>
2023-04-03 14:20:26 +0900

    민경운 : 임시저장

---
+++ APIApp/GameEngineContents/BattleState_GymBattleWin.cpp
@@ -0,0 +1,18 @@
+#include "BattleState_GymBattleWin.h"
+
+BattleState_GymBattleWin::BattleState_GymBattleWin()
+{
+
+}
+
+BattleState_GymBattleWin::~BattleState_GymBattleWin()
+{
+
+}
+
+void BattleState_GymBattleWin::EnterState()
+{
+	BattleState_TalkBase::EnterState();
+
+
+}
---
+++ APIApp/GameEngineContents/BattleState_GymBattleWin.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "BattleState_TalkBase.h"
+
+class BattleState_GymBattleWin : public BattleState_TalkBase
+{
+public:
+	BattleState_GymBattleWin();
+	~BattleState_GymBattleWin() override;
+
+	BattleState_GymBattleWin(const BattleState_GymBattleWin& _Other) = delete;
+	BattleState_GymBattleWin(BattleState_GymBattleWin&& _Other) noexcept = delete;
+	BattleState_GymBattleWin& operator=(const BattleState_GymBattleWin& _Other) = delete;
+	BattleState_GymBattleWin& operator=(const BattleState_GymBattleWin&& _Other) noexcept = delete;
+
+protected:
+	void EnterState() override;
+	
+
+private:
+
+
+};
+
