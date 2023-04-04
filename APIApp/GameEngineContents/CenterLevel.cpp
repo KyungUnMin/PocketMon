edaa84e1191843aa14ed29d018e727de52e8c931
엄태건 <umtimos@naver.com>
2023-04-04 15:17:51 +0900

    엄태건 : 사운드 업데이트

--- APIApp/GameEngineContents/CenterLevel.cpp
+++ APIApp/GameEngineContents/CenterLevel.cpp
@@ -469,6 +469,14 @@ void CenterLevel::Loading()
 		Dir.Move("BGM");
 		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("Battle_WildBGM.mp3"));             // 인배틀 BGM
 
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("NormalDamage.mp3"));               // 공격 시 노말 데미지일 때 사운드
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("SuperEffectiveDamage.mp3"));       // 효과는 굉장했다 사운드
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("PokemonSwapout.mp3"));             // 포켓몬 교체 시 퇴장 사운드
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("fainting.mp3"));				     // 포켓몬 기절 (쓰러뜨림)
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("Running.mp3"));				     // 야생 포켓몬에게서 도주
+
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("EXPGain.mp3"));				     // 경험치 획득
+
 		Dir.MoveParentToDirectory("SFX");
 		Dir.Move("SFX");
 		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName(BattleDefine::SfxName_Tackle));
@@ -481,6 +489,8 @@ void CenterLevel::Loading()
 		//UI 사운드 로드하는곳이에요
 		Dir.MoveParentToDirectory("FieldUI"); 
 		Dir.Move("FieldUI");
+
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("UsePotion.wav"));                // 포션 사용했을 때 체력회복 사운드
 
 		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("MainMenuOpen.wav"));             // 필드 UI 창 오픈
 		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("MenuButton.wav"));               // 메뉴 버튼 사운드 (이동)
@@ -498,8 +508,23 @@ void CenterLevel::Loading()
 		Dir.MoveParentToDirectory("FieldUI");
 		Dir.Move("FieldBGM");
 
-		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("Road_1.mp3"));                   // 1번 도로
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("Route_1.mp3"));                  // 1번 도로
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("PalletTown.mp3"));               // 태초 마을
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("ViridianCity.mp3"));             // 상록시티
+
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("OakLap.mp3"));				   // 오박사 실험실
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("PokemonCenter.mp3"));			   // 포켓몬 센터 
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("World_Gym.mp3"));			       // 체육관
+
 		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("GetFirstPokemon.mp3"));          // 스타팅 포켓몬 선택
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("MeetOak.mp3"));				   // 오박사 조우
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("RivalAppears.mp3"));			   // 라이벌 조우
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("Wild_Victory.mp3"));			   // 전투 승리 시 BGM
+
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("WoodDoorOpen.mp3"));			   // 나무문 열기
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("SlideDoorOpen.mp3"));			   // 슬라이드문 열기
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("ExitBuilding.mp3"));			   // 건물(빌딩) 나가기
+		GameEngineResources::GetInst().SoundLoad(Dir.GetPlusFileName("BikeBell.mp3"));			       // 자전거 벨 울리기
 	}
 
 	if (false == GameEngineInput::IsKey("LevelChange"))
